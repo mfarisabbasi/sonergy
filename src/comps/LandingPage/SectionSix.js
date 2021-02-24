32a642a8189e030fe970a36f80935cd60402c535
Faris Abbasi <mfarisabbasi@gmail.com>
2021-02-24 14:58:56 -0800

    added backend for appointment form

--- src/comps/LandingPage/SectionSix.js
+++ src/comps/LandingPage/SectionSix.js
@@ -1,10 +1,85 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import "../../styles/LandingPage/section6.css";
 import Aos from "aos";
+import { db } from "../../firebase/config";
+import ButtonLoader from "../loader";
+import cogoToast from "cogo-toast";
+import "../../styles/LandingPage/section6.css";
 import "aos/dist/aos.css";
 
 const SectionSix = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (!name || !email || !number || !message) {
+      cogoToast.error("Please fill out all the fields", {
+        position: "top-right",
+        hideAfter: 6,
+      });
+      setLoading(false);
+    } else {
+      await db
+        .collection("appointments")
+        .where("email", "==", email)
+        .get()
+        .then((snap) => {
+          if (snap.empty) {
+            db.collection("appointments")
+              .add({
+                name,
+                email,
+                number,
+                message,
+              })
+              .then(() => {
+                cogoToast.success("Message has been submitted 👍", {
+                  position: "top-right",
+                  hideAfter: 5,
+                });
+                setLoading(false);
+                setName("");
+                setEmail("");
+                setNumber("");
+                setMessage("");
+              })
+              .catch((error) => {
+                cogoToast.error(error.message, {
+                  position: "top-right",
+                  hideAfter: 5,
+                });
+                setLoading(false);
+              });
+          } else {
+            cogoToast.warn(
+              "You already submitted, Please wait for our response",
+              {
+                position: "top-right",
+                hideAfter: 5,
+              }
+            );
+            setLoading(false);
+            setName("");
+            setEmail("");
+            setNumber("");
+            setMessage("");
+          }
+        })
+        .catch((err) => {
+          cogoToast.error(err);
+        });
+    }
+    setName("");
+    setEmail("");
+    setNumber("");
+    setMessage("");
+  };
+
   useEffect(() => {
     Aos.init({});
   }, []);
@@ -21,21 +96,42 @@ const SectionSix = () => {
         </Row>
         <Row className="mt-5">
           <Col className="text-center" xs={12} sm={12} md={12} lg={12} xl={12}>
-            <form>
-              <input type="text" placeholder="Full Name" />
+            <form onSubmit={submitHandler}>
+              <input
+                type="text"
+                placeholder="Full Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></input>
               <br />
-              <input type="email" placeholder="Valid Email" />
-              <input type="tel" placeholder="Phone Number" />
+              <input
+                type="email"
+                placeholder="Valid Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+              <input
+                type="tel"
+                placeholder="Phone Number"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+              ></input>
               <br />
               <textarea
                 placeholder="Your Message"
                 cols="30"
                 rows="5"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
               ></textarea>
               <br />
-              <button className="section6-btn" type="submit">
-                Submit
-              </button>
+              {loading ? (
+                <ButtonLoader />
+              ) : (
+                <button className="section6-btn" type="submit">
+                  Submit
+                </button>
+              )}
             </form>
           </Col>
         </Row>
---
+++ src/comps/loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/loader.css";
+
+const ButtonLoader = () => {
+  return (
+    <>
+      <span className="button-loader"></span>
+    </>
+  );
+};
+
+export default ButtonLoader;
