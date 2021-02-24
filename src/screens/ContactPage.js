8e53d7d82ee21dcd0e7f60c84cc35943e3423529
Faris Abbasi <mfarisabbasi@gmail.com>
2021-02-24 15:11:27 -0800

    added backend for contact us form

--- src/screens/ContactPage.js
+++ src/screens/ContactPage.js
@@ -1,8 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { db } from "../firebase/config";
+import ButtonLoader from "../comps/loader";
+import cogoToast from "cogo-toast";
 import "../styles/contact.css";
 
 const ContactPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (!name || !email || !number || !message) {
+      cogoToast.error("Please fill out all the fields", {
+        position: "top-right",
+        hideAfter: 6,
+      });
+      setLoading(false);
+    } else {
+      await db
+        .collection("contact")
+        .where("email", "==", email)
+        .get()
+        .then((snap) => {
+          if (snap.empty) {
+            db.collection("contact")
+              .add({
+                name,
+                email,
+                number,
+                message,
+              })
+              .then(() => {
+                cogoToast.success("Message has been submitted 👍", {
+                  position: "top-right",
+                  hideAfter: 5,
+                });
+                setLoading(false);
+                setName("");
+                setEmail("");
+                setNumber("");
+                setMessage("");
+              })
+              .catch((error) => {
+                cogoToast.error(error.message, {
+                  position: "top-right",
+                  hideAfter: 5,
+                });
+                setLoading(false);
+              });
+          } else {
+            cogoToast.warn(
+              "You already submitted, Please wait for our response",
+              {
+                position: "top-right",
+                hideAfter: 5,
+              }
+            );
+            setLoading(false);
+            setName("");
+            setEmail("");
+            setNumber("");
+            setMessage("");
+          }
+        })
+        .catch((err) => {
+          cogoToast.error(err);
+        });
+    }
+    setName("");
+    setEmail("");
+    setNumber("");
+    setMessage("");
+  };
+
   return (
     <Container>
       <Row>
@@ -23,22 +98,44 @@ const ContactPage = () => {
           xl={6}
         >
           <h3>Fill Out The Form Below</h3>
-          <form>
-            <input className="contact-form" type="text" placeholder="Name" />
-            <input className="contact-form" type="email" placeholder="Email" />
+          <form onSubmit={submitHandler}>
+            <input
+              className="contact-form"
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+            <input
+              className="contact-form"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
             <input
               className="contact-form"
               type="tel"
               placeholder="Phone Number"
-            />
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+            ></input>
             <textarea
               className="contact-form-textarea"
               cols="30"
               rows="5"
               placeholder="What do you need?(Comments)"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
             <br />
-            <input className="contact-form-btn" type="submit" value="Submit" />
+            {loading ? (
+              <ButtonLoader />
+            ) : (
+              <button className="contact-form-btn" type="submit">
+                Submit
+              </button>
+            )}
           </form>
         </Col>
         <Col className="contact-r" xs={12} sm={12} md={12} lg={6} xl={6}>
